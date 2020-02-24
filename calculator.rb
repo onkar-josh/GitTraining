a8235af206c914e74ed1b3947fd2134db8c359fd
Rohit Joshi <rohit.joshi@joshsoftware.com>
2020-02-24 12:09:26 +0530

    Add divide method

--- calculator.rb
+++ calculator.rb
@@ -8,6 +8,18 @@ class Calculator
   end
 end
 
-calc = Calculator.new
-calc.addition(5,10)
-calc.squre(5)
+  def divide(operand1, operand2)
+    begin
+      puts (operand1 / operand2)
+    rescue
+      puts "Division by zero is not allowed"    
+    end
+  end
+
+  calc = Calculator.new
+  calc.addition(5,10)
+  calc.squre(5)
+  calc.divide(6,2)
+  calc.divide(6,0)
+end
+
